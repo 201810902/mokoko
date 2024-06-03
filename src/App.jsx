dbe1d54b6ebbefd0a837c2db66d5cf84d7caf780
kyounga_laptop <kacomtr@gmail.com>
2024-06-04 03:27:53 +0900

    로그인 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Community from "./pages/Community";
 import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/community" element={<Community />} />
         <Route path="/login" element={<Login />} />
+        <Route path="mypage" element={<Mypage />} />
       </Routes>
     </>
   );
