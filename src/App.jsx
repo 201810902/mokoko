00607cea6d1fe435ee5bbb1073e0e3a3ad766ec9
kyounga_laptop <kacomtr@gmail.com>
2024-06-14 23:36:07 +0900

    ✨ [feat] 캐릭터 정보 데이터 파일 및 캐릭터 정보 조회 페이지 추가 - 수정 필요

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Mypage from "./pages/Mypage";
 import SignUp from "./pages/SignUp";
 import FindId from "./pages/FindId";
+import ViewCharacter from "./pages/ViewCharacter";
 import "./App.css";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="mypage" element={<Mypage />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="findid" element={<FindId />} />
+        <Route path="viewCharacter" element={<ViewCharacter />} />
       </Routes>
     </>
   );
