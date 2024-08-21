747e80f0467a47e1f4fd823b124cfc5961ea3441
kyounga_laptop <kacomtr@gmail.com>
2024-08-21 23:40:58 +0900

    ✨ [Feat] 작성한 글 firestore에 업로드하는 기능 추가. sidemenu, 프로필(받아온 사용자정보) UI 추가 필요

--- firebase.js
+++ firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,7 +24,8 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const authService = getAuth(firebaseApp);
-
+export const dbService = getFirestore(firebaseApp);
+export const fbStorage = getStorage(firebaseApp);
 // export default firebaseConfig;
 export default firebaseApp;
 // export const firebaseInstance = getAuth();
